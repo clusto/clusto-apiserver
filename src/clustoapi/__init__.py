2d83995ae14df2f761b36a7d8cb7482c765c6c71
Jorge Gallegos <kad@blegh.net>
2013-10-27 21:36:39 -0700

    Propagate these upwards, so you only import base

---
+++ src/clustoapi/__init__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+#
+# -*- mode:python; sh-basic-offset:4; indent-tabs-mode:nil; coding:utf-8 -*-
+# vim:set tabstop=4 softtabstop=4 expandtab shiftwidth=4 fileencoding=utf-8:
+#
+import server
+import apps
+
+# Placate PEP8
+assert server
+assert apps
