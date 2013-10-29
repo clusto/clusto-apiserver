2007a3d83e653d44f753a6ce24eb48536729ca88
Jorge Gallegos <kad@blegh.net>
2013-10-28 20:41:51 -0700

    More stuff to commit

--- test/all.py
+++ test/all.py
@@ -3,7 +3,6 @@
 # -*- mode:python; sh-basic-offset:4; indent-tabs-mode:nil; coding:utf-8 -*-
 # vim:set tabstop=4 softtabstop=4 expandtab shiftwidth=4 fileencoding=utf-8:
 #
-from __future__ import absolute_import
 
 import sys
 import suites
