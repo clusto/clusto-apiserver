81c64081f937e0cd47f47a1b0913d60a8b5a1188
Jorge Gallegos <kad@blegh.net>
2013-11-26 18:19:51 -0800

    No need for this nonsense

--- clustoapi/apps/__init__.py
+++ clustoapi/apps/__init__.py
@@ -4,13 +4,8 @@
 # vim:set tabstop=4 softtabstop=4 expandtab shiftwidth=4 fileencoding=utf-8:
 #
 
-import os
-
-
-this_dir = os.path.dirname(os.path.realpath(__file__))
-__all__ = []
-for f in os.listdir(this_dir):
-    if f.startswith('__init__.'):
-        continue
-    if f.endswith('.py'):
-        __all__.append(f.split('.')[0])
+__all__ = (
+    'attribute',
+    'entity',
+    'resourcemanager'
+)
