0a609ac3b78b92e97c1269225f4fe7b203dcf841
Jorge Gallegos <kad@blegh.net>
2014-08-07 16:19:52 -0700

    A little fancier way to build the version

--- clustoapi/__init__.py
+++ clustoapi/__init__.py
@@ -4,4 +4,10 @@
 # vim:set tabstop=4 softtabstop=4 expandtab shiftwidth=4 fileencoding=utf-8:
 #
 
-__version__ = '0.1.1'
+__major__ = 0
+__minor__ = 1
+__release__ = 1
+__dotbranch__ = (__major__, __minor__,)
+__branch__ = u'.'.join([u'%s' % (_,) for _ in __dotbranch__])
+__dotversion__ = (__major__, __minor__, __release__,)
+__version__ = u'.'.join([u'%s' % (_,) for _ in __dotversion__])
