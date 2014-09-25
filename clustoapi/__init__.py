2cf35a0d9816fa35d152cc16df3ab3e744c3c8bd
Jorge Gallegos <kad@blegh.net>
2014-09-25 13:37:30 -0700

    Preparing for release

--- clustoapi/__init__.py
+++ clustoapi/__init__.py
@@ -5,8 +5,8 @@
 #
 
 __major__ = 0
-__minor__ = 1
-__release__ = 1
+__minor__ = 2
+__release__ = 0
 __dotbranch__ = (__major__, __minor__,)
 __branch__ = u'.'.join([u'%s' % (_,) for _ in __dotbranch__])
 __dotversion__ = (__major__, __minor__, __release__,)
