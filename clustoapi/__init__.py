d64cbc35039332eedca15546207e8b14d3458820
Jorge Gallegos <kad@blegh.net>
2015-08-21 12:23:48 -0700

    Bumping version for release

--- clustoapi/__init__.py
+++ clustoapi/__init__.py
@@ -6,7 +6,7 @@
 
 __major__ = 0
 __minor__ = 5
-__release__ = 1
+__release__ = 2
 __dotbranch__ = (__major__, __minor__,)
 __branch__ = u'.'.join([u'%s' % (_,) for _ in __dotbranch__])
 __dotversion__ = (__major__, __minor__, __release__,)
